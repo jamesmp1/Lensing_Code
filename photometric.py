dd35ea2fba4c19cd062b986a000ddebaa0873a2a
James Mackenzie Page <jtmp1@st-andrews.ac.uk>
2025-07-03 10:59:39 +0200

    Added program to simulate lensing light curves.

--- photometric.py
+++ photometric.py
@@ -1,9 +1,7 @@
 import numpy as np
 import time as t
-import os
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-import ffmpeg
+from tqdm import tqdm
 
 def asymptote_poi(params, l_x, l_y, l_z):
     p, ecc, theta0, phi0, sign = params
@@ -19,8 +17,8 @@ def asymptote_poi(params, l_x, l_y, l_z):
     y = r*np.sin(theta)*np.sin(phi0)
     z = r*np.cos(theta)
     u = np.sqrt(l_x**2+l_y**2)*theta_E
-    if u < 0.0001:
-        u = 0.0001
+    # if u < 0.0001:
+    #     u = 0.0001
     mag = 0.5*((u**2+2)/(u*np.sqrt(u**2+4))+sign)
     return x, y, z, mag
 
@@ -63,9 +61,11 @@ def cluster_source(x0, y0, z0, radius, r_space, theta_space, phi_space):
                 points.append(point)
     return points
 
-to_centre = True
-length = 150
-l_x_max = 20
+start_time = t.time()
+
+to_centre = False
+res = 100
+l_x_max = 0.025
 
 r_s = 2
 l_z = 500
@@ -76,52 +76,30 @@ source_size = 1
 theta_E = np.sqrt(2*r_s*l_z/(l_o*(l_o+l_z)))
 r_E = theta_E*(l_o+l_z)
 
-def update(num):
-    ax.cla()
+if to_centre:
+    l_xs = np.linspace(0, l_x_max, res)
+else:
+    l_xs = np.linspace(-l_x_max, l_x_max, res)
 
-    ax.set_title("Strong Gravitational Lensing")
-    ax.set_ylabel(r"Y ($θ_E$)")
-    ax.set_xlabel(r"X ($θ_E$)")
-    ax.set_aspect("equal")
-    ax.set_xlim(-1.5, 1.5)
-    ax.set_ylim(-1.5, 1.5)
-
-    if to_centre:
-        l_x = l_x_max*(length-num-0.5)/length
-    else:
-        l_x = l_x_max*(0.5*length-num-0.5)/(0.5*length)
-
+mags = []
+for i in tqdm(range(res)):
+    l_x = l_xs[i]
     source = cluster_source(l_x, l_y, l_z, source_size, source_size/3, np.pi/12, np.pi/12)
-
-    im1_x = []
-    im1_y = []
     mag1 = 0
-    im2_x = []
-    im2_y = []
     mag2 = 0
     for point in source:
         x, y, z = point
         params1, params2 = rays_from_point_params(r_s, x, y, z, l_o)
-        poi1 = asymptote_poi(params1, l_x, l_y, l_z)
-        poi2 = asymptote_poi(params2, l_x, l_y, l_z)
-        im1_x.append(poi1[0]/((l_o+l_z)*theta_E))
-        im2_x.append(poi2[0]/((l_o+l_z)*theta_E))
-        im1_y.append(poi1[1]/((l_o+l_z)*theta_E))
-        im2_y.append(poi2[1]/((l_o+l_z)*theta_E))
-        mag1 += poi1[3]
-        mag2 += poi2[3]
-    
-    ax.scatter(im1_x, im1_y, c="blue", s=40*mag1/(mag1+mag2), label="lensed image 1")
-    ax.scatter(im2_x, im2_y, c="blue", s=40*mag2/(mag1+mag2), label="lensed image 2")
-    ax.scatter(l_x/((l_o+l_z)*theta_E), l_y/((l_o+l_z)*theta_E), c="red", label="unlensed image")
-    ax.scatter(0, 0, c="black", marker="x", label="lens")
-    circle = plt.Circle((0,0), 1, color="green", fill=False, label="Einstein radius")
-    ax.add_patch(circle)
+        mag1 += asymptote_poi(params1, l_x, l_y, l_z)[3]
+        mag2 += asymptote_poi(params2, l_x, l_y, l_z)[3]
+        # mag1 += poi1[3]
+        # mag2 += poi2[3]
+    mags.append(mag1+mag2)
 
-    ax.legend(loc="upper right")
+print("Runtime:", t.time()-start_time)
 
-fig, ax = plt.subplots()
+_, ax = plt.subplots()
 
-ani = FuncAnimation(fig = fig, func = update, frames = length, interval = 100, repeat = False)
+ax.scatter(l_xs, mags)
 
 plt.show()
